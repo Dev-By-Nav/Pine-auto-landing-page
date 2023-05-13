c23411b25f6ca7da93d1243162ae856898903a5f
Navneet <itsnav3@gmail.com>
2023-05-13 20:40:38 +0100

    cahnged size of car logos

--- src/Components/Cars.jsx
+++ src/Components/Cars.jsx
@@ -13,14 +13,14 @@ import RollsRoyce from '../assets/Logo/Rolls-Royce_logo.png'
 const Cars = () => {
   return (
     <div className='grid grid-cols-4 lg:grid-cols-8 gap-4  '>
-        <img src={Audi} alt="" className='px-7' />
-        <img src={Bentley} alt="" className='px-5 mt-2' />
-        <img src={BMW} alt="" className='px-8'/>
-        <img src={Jaguar} alt="" className='px-8'/>
-        <img src={Lamborghini} alt="" className='px-8'/>
-        <img src={Mercedes} alt="" className='px-8'/>
-        <img src={Porsche} alt="" className='px-8'/>
-        <img src={RollsRoyce} alt="" className='px-8'/>
+        <img src={Audi} alt="" className='px-5' />
+        <img src={Bentley} alt="" className='px-2 mt-2' />
+        <img src={BMW} alt="" className='px-7'/>
+        <img src={Jaguar} alt="" className='px-6'/>
+        <img src={Lamborghini} alt="" className='px-7'/>
+        <img src={Mercedes} alt="" className='px-6'/>
+        <img src={Porsche} alt="" className='px-7'/>
+        <img src={RollsRoyce} alt="" className='px-6'/>
     </div>
   )
 }
